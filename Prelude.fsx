26c029a767022a08312a25cdf452efa3eb000297
Aryan Ebrahimpour <aryanebrahimpour@gmail.com>
2019-04-14 20:01:02 +0430

    Add exersice 2B_c

--- Prelude.fsx
+++ Prelude.fsx
@@ -1,10 +1,12 @@
 #r "packages/MathNet.Numerics/lib/net40/MathNet.Numerics.dll"
 #r "packages/MathNet.Numerics.FSharp/lib/net45/MathNet.Numerics.FSharp.dll"
 #r "packages/MathNet.Spatial/lib/net40/MathNet.Spatial.dll"
+#r "packages/MathNet.Symbolics/lib/net45/MathNet.Symbolics.dll"
 
 
 open MathNet.Numerics
 open MathNet.Numerics.LinearAlgebra
+open MathNet.Symbolics
 
 fsi.AddPrinter(fun (matrix:Matrix<float>) -> matrix.ToString())
 fsi.AddPrinter(fun (matrix:Matrix<float32>) -> matrix.ToString())
@@ -14,3 +16,5 @@ fsi.AddPrinter(fun (vector:Vector<float>) -> vector.ToString())
 fsi.AddPrinter(fun (vector:Vector<float32>) -> vector.ToString())
 fsi.AddPrinter(fun (vector:Vector<complex>) -> vector.ToString())
 fsi.AddPrinter(fun (vector:Vector<complex32>) -> vector.ToString())
+
+fsi.AddPrinter Infix.format
---
+++ src/Exersice_2B_c.fsx
@@ -0,0 +1,38 @@
+#load "../Prelude.fsx"
+#load "AryanUtilities.fsx"
+
+open MathNet.Numerics
+open MathNet.Symbolics
+open MathNet.Numerics.LinearAlgebra
+
+open Operators
+
+let r11, r12, r13, r21, r22, r23, r31, r32, r33 =
+    symbol "r11",
+    symbol "r12",
+    symbol "r13",
+    symbol "r21",
+    symbol "r22",
+    symbol "r23",
+    symbol "r31",
+    symbol "r32",
+    symbol "r33"
+
+let x, y, z = symbol "x", symbol "y", symbol "z"
+
+let zero = Number BigRational.Zero
+let one  = Number BigRational.One
+
+let T = [[r11;  r12;  r13;  x]
+         [r21;  r22;  r23;  y]
+         [r31;  r32;  r33;  z]
+         [zero; zero; zero; one]]
+
+// Note: Inversing a symbolic matrix is tough task, It is possible to do it in Math.NET classic,
+// but there is no support for it in the new library.
+// I couldn't even make it in MATLAB. Any suggestions for symbolic inverse?
+// Send a PR if you have any, thank you.
+
+// let answer = T.Inverse()
+// printfn "%A" answer
+
