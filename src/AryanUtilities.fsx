7f4ded245cb147a32fab3b9e1507cd9124e2d641
Aryan Ebrahimpour <aryanebrahimpour@gmail.com>
2019-04-14 13:34:31 +0430

    Update utils

--- src/AryanUtilities.fsx
+++ src/AryanUtilities.fsx
@@ -9,3 +9,17 @@ module Rotations =
     let rotx degree = Angle.FromDegrees degree |> Matrix3D.RotationAroundXAxis
     let roty degree = Angle.FromDegrees degree |> Matrix3D.RotationAroundYAxis
     let rotz degree = Angle.FromDegrees degree |> Matrix3D.RotationAroundZAxis
+
+module Math =
+    open System
+
+    let pi = Math.PI
+    let inline negate x = x * -1.
+    let toDegree rad = rad * (180. / pi)
+    let toRad deg = deg * (pi / 180.)
+    let cosd = toRad >> cos
+    let sind = toRad >> sin
+    let asind = toRad >> asin
+    let acosd = toRad >> acos
+    let atand = toRad >> atan
+    let atan2d = toRad >> atan2
