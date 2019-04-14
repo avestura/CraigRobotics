0c0776ff8074f174ac2853ebe4c2c1a9a3833840
Aryan Ebrahimpour <aryanebrahimpour@gmail.com>
2019-04-14 13:34:43 +0430

    Add exersice 2A-b

---
+++ src/Exersice_2A_b.fsx
@@ -0,0 +1,33 @@
+#load "../Prelude.fsx"
+#load "AryanUtilities.fsx"
+
+open AryanUtilities.Rotations
+open AryanUtilities.Math
+
+// This script calculates the Inverse (alpha-beta-gamma) of a
+// Rotation matrix and shows both possible solutions
+
+let R1 = (rotz 10.) * (roty 20.) * (rotx 30.)
+let R2 = (rotz 30.) * (roty 90.) * (rotx -55.)
+
+let beta  = R1.[3, 1] |> asind |> negate
+let alpha = R1.[1, 1] / cosd beta |> acosd
+let gamma = R1.[3, 3] / cosd beta |> acosd
+
+let answer1 = (alpha, beta, gamma)
+
+let beta2 = R2.[3, 1] |> asind |> negate
+
+let answer2 =
+    match (beta, beta2) with
+    | (-90. , 90.) ->
+      let gamma2 = atan2d R2.[1, 2] R2.[2, 2]
+      (0., beta2, gamma2)
+
+    | _ ->
+      let alpha2 = R2.[1, 1] / cosd beta2 |> acosd
+      let gamma2 = R2.[3, 3] / cosd beta2 |> acosd
+      (alpha2, beta2 |> negate, gamma2)
+
+printfn "Answer1: %A" answer1
+printfn "Answer2: %A" answer2
