ff772eeca0d43305b4f5165c32057b7ba4aa53db
Aryan Ebrahimpour <aryanebrahimpour@gmail.com>
2019-05-19 01:54:09 +0430

    FSAC temp fix for FSharp.Compiler.Interactive.Settings

--- src/Prelude.fsx
+++ src/Prelude.fsx
@@ -3,6 +3,10 @@
 #r "../packages/MathNet.Spatial/lib/net40/MathNet.Spatial.dll"
 #r "../packages/MathNet.Symbolics/lib/net45/MathNet.Symbolics.dll"
 
+// Temporary fix for FSAC bug: https://github.com/fsharp/FsAutoComplete/issues/227
+// Uncomment line below if Ionide can not find "fsi" and replace it with where fsi.exe located
+// #r "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/common7/IDE/CommonExtensions/Microsoft/FSharp/FSharp.Compiler.Interactive.Settings.dll"
+
 
 open MathNet.Numerics
 open MathNet.Numerics.LinearAlgebra
