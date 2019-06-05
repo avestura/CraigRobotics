0036dfdbabea2f9f8ba785107c10e25f844d57eb
Aryan Ebrahimpour <aryanebrahimpour@gmail.com>
2019-06-05 06:07:30 +0430

    Update Exersice_3b.fsx

--- src/Chapter3/Exersice_3b.fsx
+++ src/Chapter3/Exersice_3b.fsx
@@ -1,5 +1,34 @@
 #load "../AryanUtilities.fsx"
 
-open AryanUtilities.DenavitHartenberg
+open AryanUtilities.Math
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Symbolics
+open Operators
 
-// Todo
+let alpha, a, theta, d = 
+    symbol "alpha",
+    symbol "a",
+    symbol "theta",
+    symbol "d"
+
+let alphaRotateX = [[one ; zero        ; zero       ; zero]
+                    [zero; cos alpha   ; -sin alpha ; zero]
+                    [zero; sin alpha   ; cos alpha  ; zero]
+                    [zero; zero        ; zero       ; one]]
+
+let xOffset      = [[one ; zero; zero; a   ]
+                    [zero; one ; zero; zero]
+                    [zero; zero; one ; zero]
+                    [zero; zero; zero; one ]]
+
+let thetaRotateZ = [[cos theta   ; -sin theta   ; zero; zero]
+                    [sin theta   ;  cos theta   ; zero; zero]
+                    [zero        ;  zero        ; one ; zero]
+                    [zero        ;  zero        ; zero; one ]]    
+
+let zOffset      = [[one ; zero; zero; zero]
+                    [zero; one ; zero; zero]
+                    [zero; zero; one ; d   ]
+                    [zero; zero; zero; one ]]                       
+
+alphaRotateX * xOffset * thetaRotateZ * zOffset
